d28098272ce643f601a49d57820cab399327eafb
choco-melk <hullezachristianjave@gmail.com>
2026-03-11 15:30:22 +0800

    style: added final touches in delete modal

--- my-seasonal-anime-list/client/src/components/EditAnimeModal.jsx
+++ my-seasonal-anime-list/client/src/components/EditAnimeModal.jsx
@@ -62,7 +62,7 @@ function EditAnimeModal({ isVisible = true, onClose, anime, onEditSuccess }) {
             <FontAwesomeIcon icon={faXmark} className="modal-close-icon" onClick={onCloseButtonClick}/>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="form-element">
-                    <label htmlFor="title-input">Title: {anime.title}</label>
+                    <label htmlFor="title-input">{anime.title}</label>
                 </div>
                 <div className="form-element">
                     <label htmlFor="watched-checkbox">Watched</label>
